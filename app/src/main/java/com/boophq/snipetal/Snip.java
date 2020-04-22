ef55487da6662f2802c5f0614ef9e16b288ed08c
sixpeteunder <plenjo15@gmail.com>
2020-04-23 00:04:22 +0300

    Set up Room Entity

---
+++ app/src/main/java/com/boophq/snipetal/Snip.java
@@ -0,0 +1,42 @@
+package com.boophq.snipetal;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "snip_table")
+public class Snip {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String subject;
+
+    private String content;
+
+    private int priority;
+
+    public Snip(String subject, String content, int priority) {
+        this.subject = subject;
+        this.content = content;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
